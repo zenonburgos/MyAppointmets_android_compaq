1934b483c171a2075f38380494eb56f28c06f0fc
Zenón Burgos <znburgos@gmail.com>
2021-07-10 15:44:01 -0600

    Registro

--- app/src/main/java/solucionesnegocios/com/MainActivity.kt
+++ app/src/main/java/solucionesnegocios/com/MainActivity.kt
@@ -1,11 +1,22 @@
 package solucionesnegocios.com
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvGoToRegister.setOnClickListener {
+            Toast.makeText(this, getString(R.string.pleas_fill_your_register_data), Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
