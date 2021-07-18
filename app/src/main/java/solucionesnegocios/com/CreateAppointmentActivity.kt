7a2c3be1ab4dfb3bab9225606e2eb57cf11c9a07
Zenón Burgos <znburgos@gmail.com>
2021-07-18 08:56:18 -0600

    dateDataPicker agregado

--- app/src/main/java/solucionesnegocios/com/CreateAppointmentActivity.kt
+++ app/src/main/java/solucionesnegocios/com/CreateAppointmentActivity.kt
@@ -1,11 +1,13 @@
 package solucionesnegocios.com
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_create_appointment.*
+import java.util.*
 
 class CreateAppointmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +29,17 @@ class CreateAppointmentActivity : AppCompatActivity() {
 
         val doctorOptions = arrayOf("Doctor A", "Doctor B", "Doctor C")
         spinnerDoctors.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, doctorOptions)
+    }
+
+    fun onClickScheduledDate(v: View?){
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val listener = DatePickerDialog.OnDateSetListener { datePicker, y, m, d ->
+            Toast.makeText(this, "$y-$m-$d", Toast.LENGTH_SHORT).show()
+        }
+        DatePickerDialog(this, listener, year, month, dayOfMonth).show()
     }
 }
